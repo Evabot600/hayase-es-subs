83823de94c8c2e60b36dedadcfb3da72c2c557db
Evabot600 <facundocuadrado461@gmail.com>
2026-07-27 04:37:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-export default new class extends SubtitleSource {
-
+export default new class HayaseESSubs {
   async test() {
     return true
   }
 
   async single({ anilistId, episode, titles }) {
     console.log(anilistId, episode, titles)
-
     return []
   }
-
 }()
